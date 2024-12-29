5dacf9d53f8720cb22b9ba9dbb2ca6dc54881c03
BugHunter-X <askzenai@gmail.com>
2024-12-29 18:52:04 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int a = 5;
+  int b = 0;
+  if (b != 0) {
+    int c = a / b;
+    std::cout << "Result: " << c << std::endl;
+  } else {
+    std::cerr << "Error: Division by zero!\n";
+    return 1; // Indicate an error
+  }
+  return 0;
+}
